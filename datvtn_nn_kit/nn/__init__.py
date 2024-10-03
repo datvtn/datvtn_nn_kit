91f687d276e856122fb54174851dce3e214693a3
Dat Viet Thanh Nguyen <thanhdatnv2712@gmail.com>
2024-10-03 11:17:19 +0700

    UPDATE nn kit

--- datvtn_nn_kit/nn/__init__.py
+++ datvtn_nn_kit/nn/__init__.py
@@ -1,11 +1,8 @@
 from .image_utils import tensor_to_img, tensor_to_img_fast, img_to_tensor, img_from_bytes
-from .download_utils import download_file_from_google_drive, load_file_from_url
 
 __all__ = [
     'tensor_to_img',
     'tensor_to_img_fast',
     'img_to_tensor',
     'img_from_bytes',
-    'download_file_from_google_drive',
-    'load_file_from_url',
 ]
