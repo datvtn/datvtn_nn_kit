6190643b0b16e16b0932807f41e7ae87f9cb75fb
Dat Viet Thanh Nguyen <thanhdatnv2712@gmail.com>
2024-10-14 14:50:08 +0700

    UPDATE image utils

--- datvtn_nn_kit/nn/image_utils.py
+++ datvtn_nn_kit/nn/image_utils.py
@@ -21,6 +21,8 @@ def img_to_tensor(imgs: Union[List[NDArray[Any]], NDArray[Any]], bgr2rgb: bool =
     """
     def _to_tensor(img: NDArray[Any], bgr2rgb: bool, float32: bool) -> torch.Tensor:
         if img.shape[2] == 3 and bgr2rgb:
+            if img.dtype == 'float64':
+                img = img.astype('float32')
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = torch.from_numpy(img.transpose(2, 0, 1))
         if float32:
